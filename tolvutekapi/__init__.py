b39d89197e58794ef91fd9ece1994eb24eeaefcb
steini <steini90@gmail.com>
2013-02-03 00:51:15 +0000

    first roll

---
+++ tolvutekapi/__init__.py
@@ -0,0 +1,193 @@
+import codecs
+from urllib import urlencode
+from cookielib import CookieJar
+from urllib2 import build_opener, HTTPCookieProcessor
+import logging
+from HTMLParser import HTMLParser
+
+from bs4 import BeautifulSoup
+
+def get_log():
+    log = logging.getLogger('tolvutek')
+    formatter = logging.Formatter(
+        '%(levelname)s:%(module)s.%(funcName)s: %(message)s'
+        )
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+    return log
+
+log = logging.getLogger('tolvutek')
+
+class TolvutekError(Exception):
+    pass
+
+class Product(object): 
+    def __init__(self, **kwargs):
+        for key,value in kwargs.iteritems():
+            if key == 'common_price' or key == 'discount_price':
+                value = int(value.replace('.',''))
+            elif isinstance(value, basestring):
+                value = unicode(value)
+            self.__setattr__(key,value)
+
+    def __str__(self):
+        s = u'{} - {} kr.'.format(
+            self.name, self.discount_price
+            )
+        return s.encode('utf-8')
+
+class Tolvutek(object):
+
+    url_base = u'http://tolvutek.is'
+    url_product_base = u'/vorur'
+    url_login = u'/login/loginsubmit'
+    url_cart = u'/karfa'
+    url_add_to_cart = u'/karfa/add_to_cart'
+
+    def __init__(self, username=None, password=None):
+        self.login = self.get_login(username, password)
+        self.products = {} #url:Product dict
+
+    def get_cart(self):
+        """
+        Get products in cart for given session.
+        """
+        csoup = self.get_soup(self.url_cart)
+        return self._extract_products(csoup, cart=True)
+
+    def add_to_cart(self, product):
+        body = {'varaId':product.add_to_cart_id}
+        resp = self.post(self.url_add_to_cart, body)
+        if resp.code >= 300:
+            raise TolvutekError(
+                u'Error occured when adding {} to cart. HTTP code: {}'.format(
+                    product, resp.code)
+                )        
+
+    def get_product(self, url, usecache=True):
+        """
+        Get a `Product` object from `soup`.
+        """
+        if usecache:
+            try:
+                return self.products[url]
+            except KeyError:
+                pass
+        h = HTMLParser()
+        soup = self.get_soup(url)
+        leftsoup = soup.find('div', 'leftcontent')
+        soup = soup.find('div', 'rightcontent')
+        info = soup.findAll('span', 'modelnr')
+
+        product = Product(
+            model_no = h.unescape(info[0].contents[0])[len('typunumer: '):],
+            catalog_no = h.unescape(info[1].contents[0])[len('Vorunumer: '):],
+            common_price = info[2].contents[0][len('agv: '):-3],
+            name = soup.find('h2').contents[0],
+            discount_price = soup.find('div', 'price').contents[0][:-3],
+            description = h.unescape(soup.find('div', 'boxinfo').contents[2]).strip(),
+            add_to_cart_id = soup.find('input').attrs['value'],
+            image_url = leftsoup.find('a', attrs={'rel':'prettyPhoto'}).attrs['href'],            
+            )    
+        self.products[url] = product
+        return product
+
+    def get_products(self, cat, subcat, subsubcat=None):
+        """
+        Get all products in given category and subcategory.
+        """
+        u = u'/{}/{}'.format(cat,subcat)
+        if subsubcat:
+            u+=u'/'+subsubcat
+        u+=u'?'
+        url = self.url_base+self.url_product_base+u
+        log.debug(u'product url: %s', url)
+        soup = self.get_soup(url)
+        pages = soup.find('div', 'paginationControl')
+        if not pages:
+            return self._extract_products(soup)            
+        pages = pages.findAll('a')
+        #strip the garbage links
+        pages = pages[2:-2]
+        products = self._extract_products(soup)        
+        for page in pages[1:]:
+            soup = self.get_soup(self.url_base+page.attrs['href'])
+            ps = self._extract_products(soup)
+            products.update(ps)
+        return products
+        
+
+    def get_soup(self, url, body=None):
+        """
+        Get a BeautifulSoup object for given url and request body.
+        """
+        url = self.get_url(url)
+        return BeautifulSoup(self._get_html(url, body=body))
+
+    def get_login(self, user, pw):
+        """
+        Get the session urlopener.
+        """
+        cj = CookieJar()
+        opener = build_opener(HTTPCookieProcessor(cj))
+        data = urlencode(
+            {'username':user,'password':pw}
+            )
+        opener.open(self.url_base+self.url_login, data)
+        return opener
+
+    def post(self, url, body):
+        """
+        Do a post and get response object.
+        """
+        url = self.get_url(url)
+        body = urlencode(body)
+        response = self.login.open(url, body)
+        return response
+        
+    def get_url(self, url):
+        """
+        Get absolute url for given url.
+        """
+        if not url.startswith(self.url_base):
+            url = self.url_base+url
+        return url
+        
+
+    def _extract_products(self, soup, cart=False):
+        """
+        Get products from given BeautifulSoup.
+        Set `cart` to True if `soup` is a cart.
+        """
+        if cart:
+            product_soups = soup.findAll('div', 'details')
+        else:
+            product_soups = soup.findAll('div', attrs={'class':'box-middle'})
+        products = {}
+        for s in product_soups:
+            purl = s.find('a').attrs['href']
+            prod = self.get_product(purl)
+            products[prod.catalog_no] = prod
+        return products
+
+    def _get_html(self, url, body=None):
+        if body is not None:
+            body = urlencode(body)
+        html = self.login.open(url, body).read()
+        html = html.decode('utf-8', 'mixed')
+        return html
+
+
+last_pos = -1
+def mixed_decoder(unicode_error):
+    global last_pos
+    string = unicode_error[1]
+    pos = unicode_error.start
+    if pos <= last_pos:
+        pos = last_pos+1
+    last_pos = pos
+    new_char = string[pos].decode('ISO-8859-1')
+    return new_char, pos+1
+
+codecs.register_error('mixed', mixed_decoder)
